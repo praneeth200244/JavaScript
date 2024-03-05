5877d611140b8bb29b8356245f78dc52fc54c67f
Praneeth Devadiga <106474034+praneeth200244@users.noreply.github.com>
2024-03-05 10:52:55 +0530

    Add files via upload

---
+++ 051_localStorage.js
@@ -0,0 +1,32 @@
+let key = prompt("Enter the key: ");
+let value = prompt("Enter the value: ");
+
+localStorage.setItem(key, value);
+
+document.write(localStorage.getItem("A"));
+localStorage.removeItem("A");
+
+document.write(localStorage.key(2));
+
+console.log(localStorage.length);
+
+localStorage.A = "ANT";
+delete localStorage.A;
+
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York",
+};
+
+const personString = JSON.stringify(person);
+localStorage.setItem("personData", personString);
+
+const storedPersonString = localStorage.getItem("personData");
+const storedPerson = JSON.parse(storedPersonString);
+
+console.log(storedPerson.name);
+console.log(storedPerson.age);
+console.log(storedPerson.city);
+
+localStorage.clear();
